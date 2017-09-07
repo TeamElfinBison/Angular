b8f6c8f117a24f59c1752260b403e86df4f6a5c3
Rosen Urkov <rosen.urkov@gmail.com>
2017-09-07 19:14:52 +0300

    Extracted project data to jsons for easy db seed

---
+++ seed-db.js
@@ -0,0 +1,314 @@
+/* eslint-disable */
+
+const products = [{
+        'dough': [{
+                'name': 'White',
+                'add': false,
+                'price': 2,
+                'id': '1-bg',
+            },
+            {
+                'name': 'Wholegrain',
+                'add': false,
+                'price': 2,
+                'id': '2-bg',
+            },
+            {
+                'name': 'Gluten Free',
+                'add': false,
+                'price': 2,
+                'id': '39-en',
+            }
+        ]
+    },
+    {
+        'sauce': [{
+                'name': 'Tomatoe Sauce',
+                'add': false,
+                'price': 0.5,
+                'id': '3-bg',
+            },
+            {
+                'name': 'Cream',
+                'add': false,
+                'price': 0.5,
+                'id': '4-bg',
+            },
+            {
+                'name': 'Barbecue',
+                'add': false,
+                'price': 0.5,
+                'id': '5-en',
+            }
+        ]
+    },
+    {
+        'cheese': [{
+                'name': 'Emmental',
+                'add': false,
+                'price': 0.5,
+                'id': '35-en',
+            },
+            {
+                'name': 'Cheddar',
+                'add': false,
+                'price': 0.5,
+                'id': '9-bg',
+            },
+            {
+                'name': 'Blue',
+                'add': false,
+                'price': 0.5,
+                'id': '34-en',
+            },
+            {
+                'name': 'Parmesan',
+                'add': false,
+                'price': 0.5,
+                'id': '8-bg',
+            },
+            {
+                'name': 'Freshmozzarella',
+                'add': false,
+                'price': 0.5,
+                'id': '33-en',
+            },
+            {
+                'name': 'White',
+                'add': false,
+                'price': 0.5,
+                'id': '7-bg',
+            },
+            {
+                'name': 'Mozzarella',
+                'add': false,
+                'price': 0.5,
+                'id': '6-bg',
+            },
+            {
+                'name': 'Melted',
+                'add': false,
+                'price': 0.5,
+                'id': '32-en',
+            }
+        ]
+    },
+    {
+        'meat': [{
+                'name': 'Pork',
+                'add': false,
+                'price': 0.5,
+                'id': '30-en',
+            },
+            {
+                'name': 'Chorizo',
+                'add': false,
+                'price': 0.5,
+                'id': '29-en',
+            },
+            {
+                'name': 'Ham',
+                'add': false,
+                'price': 0.5,
+                'id': '28-en',
+            },
+            {
+                'name': 'Sausages',
+                'add': false,
+                'price': 0.5,
+                'id': '27-en',
+            },
+            {
+                'name': 'Chicken',
+                'add': false,
+                'price': 0.5,
+                'id': '13-bg',
+            },
+            {
+                'name': 'Pepperoni',
+                'add': false,
+                'price': 0.5,
+                'id': '12-bg',
+
+            },
+            {
+                'name': 'Ventricina',
+                'add': false,
+                'price': 0.5,
+                'id': '11-bg',
+
+            },
+            {
+                'name': 'Prosciutto',
+                'add': false,
+                'price': 0.5,
+                'id': '10-bg',
+
+            },
+            {
+                'name': 'Spicybeef',
+                'add': false,
+                'price': 0.5,
+                'id': '25-en',
+
+            },
+            {
+                'name': 'Bacon',
+                'add': false,
+                'price': 0.5,
+                'id': '26-en',
+
+            }
+        ]
+    },
+    {
+        'vegetables': [{
+                'name': 'Rucola',
+                'add': false,
+                'price': 0.5,
+                'id': '20-bg',
+
+            },
+            {
+                'name': 'Tomatoes',
+                'add': false,
+                'price': 0.5,
+                'id': '14-bg',
+
+            },
+            {
+                'name': 'Mushrooms',
+                'add': false,
+                'price': 0.5,
+                'id': '15-bg',
+
+            },
+            {
+                'name': 'RedOnions',
+                'add': false,
+                'price': 0.5,
+                'id': '21-bg',
+
+            },
+            {
+                'name': 'DriledTomatoes',
+                'add': false,
+                'price': 0.5,
+                'id': '36-bg',
+
+            },
+            {
+                'name': 'Jalapenos',
+                'add': false,
+                'price': 0.5,
+                'id': '16-bg',
+
+            },
+            {
+                'name': 'Corn',
+                'add': false,
+                'price': 0.5,
+                'id': '17-bg',
+
+            },
+            {
+                'name': 'Pineapple',
+                'add': false,
+                'price': 0.5,
+                'id': '37-bg',
+
+            },
+            {
+                'name': 'Pickels',
+                'add': false,
+                'price': 0.5,
+                'id': '38-en',
+
+            },
+            {
+                'name': 'Olives',
+                'add': false,
+                'price': 0.5,
+                'id': '18-bg',
+
+            },
+            {
+                'name': 'GreenPepers',
+                'add': false,
+                'price': 0.5,
+                'id': '19-bg',
+            },
+            {
+                'name': 'RedPeppers',
+                'add': false,
+                'price': 0.5,
+                'id': '41-en',
+            }
+        ]
+    },
+];
+
+const pizza = [{
+        'name': 'Mediteraneo',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1314medium.png',
+        'price': 2,
+    },
+    {
+        'name': 'Margarita',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1265medium.png',
+        'price': 3,
+    },
+    {
+        'name': 'Carbonara',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1360medium.png',
+        'price': 1.5,
+    },
+    {
+        'name': 'American Hot',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1291medium.png',
+        'price': 2.1,
+    },
+    {
+        'name': 'Peperoni',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1293medium.png',
+        'price': 4,
+    },
+    {
+        'name': 'Hawai',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1313medium.png',
+        'price': 3.2,
+    },
+    {
+        'name': 'Meat Mania',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1364medium.png',
+        'price': 2.7,
+    },
+    {
+        'name': 'Burger Pizza',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1505medium.png',
+        'price': 3,
+    },
+    {
+        'name': 'Chorizana',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1361medium.png',
+        'price': 2.8,
+    },
+    {
+        'name': 'Pizza Ton',
+        'img-url': 'http://www.dominos.bg/gallery/fmobile/1363medium.png',
+        'price': 4.7,
+    },
+];
+
+const path = require('path');
+const fs = require('fs')
+
+fs.writeFileSync(
+    path.join(__dirname, 'products.json'),
+    JSON.stringify(products)
+);
+
+fs.writeFileSync(
+    path.join(__dirname, 'pizza.json'),
+    JSON.stringify(pizza)
+);
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -33,7 +33,7 @@ export class NavbarComponent implements OnInit, DoCheck {
         this.dialogService.addDialog(LoginComponent, { title: 'Log in' });
     }
     registerModal() {
-        this.dialogService.addDialog(RegisterComponent, { title: 'Log in' });
+        this.dialogService.addDialog(RegisterComponent, { title: 'Register' });
     }
 }
 
