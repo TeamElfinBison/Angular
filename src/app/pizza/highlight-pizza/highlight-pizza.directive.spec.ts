6d1100b498e0b20634a6c8cca119c673679a229b
Rosen Urkov <rosen.urkov@gmail.com>
2017-09-11 03:07:41 +0300

    Deleting spec files

--- src/app/pizza/highlight-pizza/highlight-pizza.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightPizzaDirective } from './highlight-pizza.directive';
-
-describe('HighlightPizzaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightPizzaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
